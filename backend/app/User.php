c2c8b231432befd95f73eb94b2de54574e1e7252
shahaj90 <gshahaj@gmail.com>
2018-05-01 11:45:24 +0600

    signup

--- backend/app/User.php
+++ backend/app/User.php
@@ -48,4 +48,13 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function setPasswordAttribute($value)
+    {
+        
+        $this->attributes['password'] = bcrypt($value);
+    }
+  
+
+
 }
